e0d35f111ff71366be785e96fe0cd6f416a894b4
vikashkumar016 <138430675+vikashkumar016@users.noreply.github.com>
2025-10-01 23:07:48 +0530

    Time: 4 ms (0.17%) | Memory: 40.7 MB (31.78%) - LeetSync

--- 1642-water-bottles/water-bottles.java
+++ 1642-water-bottles/water-bottles.java
@@ -1,18 +1,17 @@
 class Solution {
     public int numWaterBottles(int numBottles, int numExchange) {
-        int totalDrinks = numBottles;  // Start by drinking all full bottles
-        int emptyBottles = numBottles; // Initially, all full bottles become empty after drinking
-        
-        // While we can exchange empty bottles for full ones
-        while (emptyBottles >= numExchange) {
-            // Exchange empty bottles for new full bottles
-            int newFullBottles = emptyBottles / numExchange;
-            totalDrinks += newFullBottles;
-            
-            // Update the number of empty bottles
-            emptyBottles = emptyBottles % numExchange + newFullBottles;
+        int count = 0;
+        int remain = 0;
+        int store;
+        while (numBottles > 0){
+            count += numBottles;
+            store = numBottles;
+            numBottles = (int) ((numBottles + remain) / numExchange);
+            remain += store - (numBottles * numExchange);
+            System.out.println(store);
+            System.out.println(numBottles);
+            System.out.println(remain);
         }
-        
-        return totalDrinks;
-    }
+    return count;
+}
 }
