3f291f5878c51efda49d638d37c6068a3913d79a
vikashkumar016 <138430675+vikashkumar016@users.noreply.github.com>
2025-11-20 22:49:50 +0530

    Time: 4 ms (4.95%) | Memory: 42.5 MB (5.98%) - LeetSync

--- 172-factorial-trailing-zeroes/factorial-trailing-zeroes.java
+++ 172-factorial-trailing-zeroes/factorial-trailing-zeroes.java
@@ -1,13 +1,22 @@
 class Solution {
     public int trailingZeroes(int n) {
-         int count = 0;
+        //  int count = 0;
         
-        // Count the number of multiples of 5, 25, 125, etc.
-        while (n >= 5) {
-            n /= 5;  // Divide n by 5 and add to the count
-            count += n;  // Add the result to count
+        // // Count the number of multiples of 5, 25, 125, etc.
+        // while (n >= 5) {
+        //     n /= 5;  // Divide n by 5 and add to the count
+        //     count += n;  // Add the result to count
+        // }
+        
+        // return count;
+        int zeroCount = 0;
+        for (int i = 5; i <= n; i += 5) {
+            int currentFactor = i;
+            while (currentFactor % 5 == 0) {
+                zeroCount++;
+                currentFactor /= 5;
+            }
         }
-        
-        return count;
+        return zeroCount;
     }
 }
