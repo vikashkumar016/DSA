204061bb730139f9e64787b39e1cc5335d386351
vikashkumar016 <138430675+vikashkumar016@users.noreply.github.com>
2026-02-05 12:09:53 +0530

    Time: 1 ms (98.72%) | Memory: 46.8 MB (51.92%) - LeetSync

--- 3651-transformed-array/transformed-array.java
+++ 3651-transformed-array/transformed-array.java
@@ -1,24 +1,45 @@
 class Solution {
     public int[] constructTransformedArray(int[] nums) {
-        int[] result = new int[nums.length];
-        int n = nums.length;
-         for(int i=0;i<nums.length;i++){
-            if(nums[i]>0){
-                int idx = (i+nums[i])%n;
-                result[i]=nums[idx];
-            }
-            else if(nums[i]<0){
-                int step=Math.abs(nums[i]);
-                int idx =i-step;
-                if(idx<0){
-                    idx=(idx%n+n)%n;
-                }
-                result[i]=nums[idx];
-            }
-            else {
-                result[i]=nums[i];
-            }
-         }
+        /*
+| Part          | Purpose               |
+| ------------- | --------------------- |
+| `i + nums[i]` | actual movement       |
+| `% n`         | circular wrap         |
+| `+ n`         | fixes negative modulo |
+| `% n`         | final valid index     |
+
+
+
+        */
+//         int[] result = new int[nums.length];
+//         int n = nums.length;
+//          for(int i=0;i<nums.length;i++){
+//             if(nums[i]>0){
+//                 int idx = (i+nums[i])%n;
+//                 result[i]=nums[idx];
+//             }
+//             else if(nums[i]<0){
+//                 int step=Math.abs(nums[i]);
+//                 int idx =i-step;
+//                 if(idx<0){
+//                     idx=(idx%n+n)%n;
+//                 }
+//                 result[i]=nums[idx];
+//             }
+//             else {
+//                 result[i]=nums[i];
+//             }
+//          }
+//    return result;
+
+//optimised code 
+     int[] result = new int[nums.length];
+         int n = nums.length;
+
+   for(int i=0;i<n;i++){
+    int idx = ((i + nums[i]) % n + n) % n;
+    result[i]=nums[idx];
+   }
    return result;
     }
 }
