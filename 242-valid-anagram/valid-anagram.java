819b8b0929ea0b7bb48f5b2dd7e240d4074f8e34
vikashkumar016 <138430675+vikashkumar016@users.noreply.github.com>
2026-02-06 10:42:43 +0530

    Time: 4 ms (79.95%) | Memory: 44.4 MB (74.32%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,24 +1,38 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        HashMap<Character,Integer>map = new HashMap<>(); 
-         for(int i=0;i<s.length();i++){
-            char ch=s.charAt(i);
-            map.put(ch,map.getOrDefault(ch, 0)+1);
-         }
+        // HashMap<Character,Integer>map = new HashMap<>(); 
+        //  for(int i=0;i<s.length();i++){
+        //     char ch=s.charAt(i);
+        //     map.put(ch,map.getOrDefault(ch, 0)+1);
+        //  }
+        //  for(int i=0;i<t.length();i++){
+        //     char ch=t.charAt(i);
+        //     if(map.get(ch)!=null){
+        //         if(map.get(ch)==1){
+        //             map.remove(ch);
+        //         }
+        //         else {
+        //              map.put(ch,map.get(ch)-1);
+        //         }
+        //     }
+        //     else {
+        //         return false;
+        //     }
+        //  }
+        //  return map.isEmpty();
+        int[] freq=new  int[26];
+        if(s.length()!=t.length()){
+            return false;
+        }    
+        for(int i=0;i<s.length();i++){
+            freq[s.charAt(i)-'a']++;
+        }
          for(int i=0;i<t.length();i++){
-            char ch=t.charAt(i);
-            if(map.get(ch)!=null){
-                if(map.get(ch)==1){
-                    map.remove(ch);
-                }
-                else {
-                     map.put(ch,map.get(ch)-1);
-                }
-            }
-            else {
+            freq[t.charAt(i)-'a']--;
+            if(freq[t.charAt(i)-'a']<0){
                 return false;
             }
          }
-         return map.isEmpty();
+     return true;
     }
 }
