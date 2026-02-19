a7bbf8208ef601fb969813dcf85cfde02caed819
vikashkumar016 <138430675+vikashkumar016@users.noreply.github.com>
2026-02-19 11:46:16 +0530

    Time: 574 ms (8.53%) | Memory: 51.3 MB (83.85%) - LeetSync

--- 816-design-hashset/design-hashset.java
+++ 816-design-hashset/design-hashset.java
@@ -1,24 +1,50 @@
+// class MyHashSet {
+//        private HashSet<Integer> set;
+
+//     public MyHashSet() {
+//         set= new HashSet<>();
+
+//     }
+    
+//     public void add(int key) {
+//       set.add(key);
+//     }
+    
+//     public void remove(int key) {
+//         set.remove(key);
+//     }
+    
+//     public boolean contains(int key) {
+//         return set.contains(key);
+//     }
+// }
 class MyHashSet {
-       private HashSet<Integer> set;
+    private List<Integer> list;
 
     public MyHashSet() {
-        set= new HashSet<>();
-
+        list = new ArrayList<>();
     }
     
     public void add(int key) {
-      set.add(key);
+        if(!list.contains(key)) list.add(key);
     }
     
     public void remove(int key) {
-        set.remove(key);
+        list.remove(Integer.valueOf(key));
     }
     
     public boolean contains(int key) {
-        return set.contains(key);
+        return list.contains(key);
     }
 }
 
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
 /**
  * Your MyHashSet object will be instantiated and called as such:
  * MyHashSet obj = new MyHashSet();
