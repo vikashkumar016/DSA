09b4d58f69884f4522fb8ffce5e1c377296fd173
Vikash Kumar <138430675+vikashkumar016@users.noreply.github.com>
2026-03-10 21:01:53 +0530

    Time: 2 ms (94.50%) | Memory: 65.3 MB (70.80%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,48 +1,23 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         
-        // int n = nums.length;
-        // int[] result = new int[n];
-
-        // for (int i = 0; i < n; i++) {
-
-        //     int product = 1;
-
-        //     for (int j = 0; j < n; j++) {
-        //         if (i != j) {
-        //             product *= nums[j];
-        //         }
-        //     }
-
-        //     result[i] = product;
-        // }
-
-        // return result;
-
         int n = nums.length;
-        
-        int[] left = new int[n];
-        int[] right = new int[n];
-        int[] answer = new int[n];
+        int[] result = new int[n];
+      
+      int[] prefix=new int[n];
+      int[] sufix=new int[n];
+      prefix[0]=1;
+      sufix[n-1]=1;
+      for(int i=1;i<n;i++){
+        prefix[i]=prefix[i-1]*nums[i-1];
+      }
+      for(int i=n-2;i>=0;i--){
+        sufix[i]=sufix[i+1]*nums[i+1];
+      }
 
-        // Step 1: Fill left array
-        left[0] = 1;
-        for (int i = 1; i < n; i++) {
-            left[i] = left[i - 1] * nums[i - 1];
-        }
-
-        // Step 2: Fill right array
-        right[n - 1] = 1;
-        for (int i = n - 2; i >= 0; i--) {
-            right[i] = right[i + 1] * nums[i + 1];
-        }
-
-        // Step 3: Multiply left and right arrays
-        for (int i = 0; i < n; i++) {
-            answer[i] = left[i] * right[i];
-        }
-
-        return answer;
-    
+       for(int i=0;i<n;i++){
+        result[i]=prefix[i]*sufix[i];
+       }
+      return result;
     }
 }
