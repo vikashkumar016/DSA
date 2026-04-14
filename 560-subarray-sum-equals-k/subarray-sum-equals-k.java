fb38266c3fb808a506ff25f6c1fe6659f77a6642
Vikash Kumar <138430675+vikashkumar016@users.noreply.github.com>
2026-04-14 17:43:12 +0530

    Time: 20 ms (98.27%) | Memory: 49.6 MB (7.47%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -8,22 +8,22 @@ class Solution {
         for(int i = 1; i < nums.length; i++){
             prefix[i] = prefix[i-1] + nums[i];
         }
-
-        HashMap<Integer,Integer> map = new HashMap<>();
-
-        for(int i = 0; i < nums.length; i++){
-
-            if(prefix[i] == k) count++;
-
-            int val = prefix[i] - k;
-
-            if(map.containsKey(val)){
-                count += map.get(val);
+        HashMap<Integer,Integer>map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(prefix[i]==k){
+                count++;
             }
-
-            map.put(prefix[i], map.getOrDefault(prefix[i], 0) + 1);
+           int need = prefix[i]-k;
+           if(map.containsKey(need)){
+            count+=map.get(need);
+           }
+           map.put(prefix[i],map.getOrDefault(prefix[i],0)+1);
         }
-
         return count;
     }
 }
+
+
+// Main running sum maintain karta hu
+// Har step pe check karta hu ki currentSum - k pehle aaya hai ya nahi
+// Agar aaya hai toh utni baar subarray milta ha
