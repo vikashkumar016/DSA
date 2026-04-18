0b3aac9a4a22779d874130e6abef83cb99e3480a
Vikash Kumar <138430675+vikashkumar016@users.noreply.github.com>
2026-04-18 15:29:05 +0530

    Time: 1 ms (78.87%) | Memory: 42.6 MB (39.08%) - LeetSync

--- 202-happy-number/happy-number.java
+++ 202-happy-number/happy-number.java
@@ -1,22 +1,24 @@
 class Solution {
     public boolean isHappy(int n) {
-     while (n != 1) {
+      HashSet<Integer> set = new HashSet<>();
+
+        while (n != 1) {
+
+            if (set.contains(n)) {
+                return false;
+            }
+
+            set.add(n);
+
             int sum = 0;
 
-            // Step: calculate sum of squares of digits
             while (n > 0) {
                 int digit = n % 10;
                 sum += digit * digit;
                 n = n / 10;
             }
 
-            // update n for next iteration
             n = sum;
-
-            // cycle detection (important)
-            if (n == 4) {
-                return false;
-            }
         }
 
         return true;
