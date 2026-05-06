c21d0415d44c3e7ed422ccf341d86bfac34d209a
Vikash Kumar <138430675+vikashkumar016@users.noreply.github.com>
2026-05-06 21:02:25 +0530

    Time: 3 ms (98.48%) | Memory: 85 MB (23.54%) - LeetSync

--- 1206-corporate-flight-bookings/corporate-flight-bookings.java
+++ 1206-corporate-flight-bookings/corporate-flight-bookings.java
@@ -1,13 +1,17 @@
 class Solution {
     public int[] corpFlightBookings(int[][] bookings, int n) {
-        int[] ans = new int[n];
+        int[] ans = new int[n]; // Time complexity= O(bookings*n)
         for(int i=0;i<bookings.length;i++){
             int start=bookings[i][0];
             int end=bookings[i][1];
              int seats = bookings[i][2];
-            for(int j=start;j<=end;j++){
-                ans[j-1]+=seats;
-            }
+            ans[start-1]+=seats;
+               if(end<n){
+                 ans[end]-=seats;
+               }
+        }
+        for(int i=1;i<ans.length;i++){
+            ans[i]=ans[i]+ans[i-1];
         }
         return ans;
     }
