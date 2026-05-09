4f6a88beb8b2ad2bd9b026e9437814d0ab39039d
Vikash Kumar <138430675+vikashkumar016@users.noreply.github.com>
2026-05-09 13:57:53 +0530

    Time: 1 ms (56.12%) | Memory: 42.5 MB (81.29%) - LeetSync

--- 886-score-of-parentheses/score-of-parentheses.java
+++ 886-score-of-parentheses/score-of-parentheses.java
@@ -1,40 +1,23 @@
 class Solution {
     public int scoreOfParentheses(String s) {
-     Stack<Integer> stack = new Stack<>();
-
-        // outermost level score
+   Stack<Integer>stack= new Stack<>();
+     stack.push(0);
+     for(char ch:s.toCharArray()){
+      if(ch=='('){
         stack.push(0);
-
-        for (char ch : s.toCharArray()) {
-
-            // new group starts
-            if (ch == '(') {
-
-                stack.push(0);
-
-            } else {
-
-                // current group's score
-                int top = stack.pop();
-
-                int score;
-
-                // means "()"
-                if (top == 0) {
-                    score = 1;
-                }
-
-                // means "(A)"
-                else {
-                    score = 2 * top;
-                }
-
-                // add to parent group
-                stack.push(stack.pop() + score);
-            }
+      }
+      else {
+        int score;
+        int top=stack.pop();
+        if(top==0){  //found ()
+           score=1;
         }
-
-        return stack.pop();
-       
+        else {
+            score=2*top;
+        }
+        stack.push(stack.pop()+score);
+      }
+     }
+     return stack.pop();
     }
 }
