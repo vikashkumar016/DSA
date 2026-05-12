ae458683d5416bb0b7d3d135ea8b4119d8a6ee0a
Vikash Kumar <138430675+vikashkumar016@users.noreply.github.com>
2026-05-13 00:27:48 +0530

    Time: 104 ms (55.48%) | Memory: 141.7 MB (71.71%) - LeetSync

---
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
@@ -0,0 +1,33 @@
+class NumMatrix {
+   private int[][] prefix;
+    public NumMatrix(int[][] matrix) {
+        if(matrix==null || matrix.length==0 || matrix[0].length==0){
+            return;
+        }
+        int m=matrix.length;
+        int n=matrix[0].length;
+        prefix= new int[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                int top=(i>0) ? prefix[i-1][j]:0;
+                int left=(j>0) ? prefix[i][j-1]:0;
+                int topLeft=(i>0 && j>0) ? prefix[i-1][j-1] :0;
+                prefix[i][j]=matrix[i][j]+top+left-topLeft;
+            }
+        }
+    }
+    
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        int total=prefix[row2][col2];
+        int top=(row1>0) ? prefix[row1-1][col2]:0;
+        int left=(col1>0) ? prefix[row2][col1-1]:0;
+        int topLeft=(row1 >0 && col1>0) ? prefix[row1-1][col1-1]:0;
+        return total-top-left+ topLeft;
+    }
+}
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix obj = new NumMatrix(matrix);
+ * int param_1 = obj.sumRegion(row1,col1,row2,col2);
+ */
