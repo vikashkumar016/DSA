88d657c6a3ad9ac5bbdf263f5ea1b669c8d08b4e
Vikash Kumar <138430675+vikashkumar016@users.noreply.github.com>
2026-05-13 21:29:00 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (51.12%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -3,9 +3,7 @@ class Solution {
         if(n==0){
             return 0;
         }
-        if(n==1|| n==2){
-            return 1;
-        }
+    
         int[] dp= new int[n+1];
         dp[0]=0;
         dp[1]=1;
