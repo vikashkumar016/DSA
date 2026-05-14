c59f8ddb4e9e97db4b94d787348a58b4fd44dd0d
Vikash Kumar <138430675+vikashkumar016@users.noreply.github.com>
2026-05-14 23:48:43 +0530

    Time: 32 ms (7.29%) | Memory: 46.3 MB (79.43%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,39 +1,17 @@
 class Solution {
     public int maxVowels(String s, int k) {
-    int count=0;
-    int max=0;
-     for(int i=0;i<k;i++){
-     char ch=s.charAt(i);
-     if(ch == 'a' || ch == 'e' || ch == 'i' ||
-               ch == 'o' || ch == 'u') {
-
-                count++;
-            }
-     }        
-     max=count;
-     int left=0;
-     for(int right=k;right<s.length();right++){
-
-        // remove left char
-           char leftChar= s.charAt(left);
-
-         if(leftChar == 'a' || leftChar == 'e' || leftChar == 'i' ||
-               leftChar == 'o' || leftChar == 'u') {
-
-                count--;
-            }
-              // add right character
-            char rightChar = s.charAt(right);
-
-            if(rightChar == 'a' || rightChar == 'e' || rightChar == 'i' ||
-               rightChar == 'o' || rightChar == 'u') {
-
-                count++;
-            }
-            max=Math.max(count,max);
-            left++;
-
-     }
-     return max;
+    Set<Character> vowels = Set.of('a', 'e', 'i', 'o', 'u');
+        int count = 0;
+        for (int i = 0; i < k; i++) {
+            count += vowels.contains(s.charAt(i)) ? 1 : 0;
+        }
+        int answer = count;
+        for (int i = k; i < s.length(); i++) {
+            count += vowels.contains(s.charAt(i)) ? 1 : 0;
+            count -= vowels.contains(s.charAt(i - k)) ? 1 : 0;
+            answer = Math.max(answer, count);
+        }
+        
+        return answer;
     }
 }
