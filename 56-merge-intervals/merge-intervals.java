cbe3633639caef5f824940b9b41d56b2f18c0dbb
Vikash Kumar <138430675+vikashkumar016@users.noreply.github.com>
2026-06-03 22:28:59 +0530

    Time: 9 ms (34.88%) | Memory: 49 MB (70.80%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -4,17 +4,18 @@ class Solution {
       List<int[]> list = new ArrayList<>();
        int start =intervals[0][0];
        int end =intervals[0][1];
-       for(int i=1;i<intervals.length;i++){
-        if(intervals[i][0]<=end){
+     for(int i=1;i<intervals.length;i++){
+        if(intervals[i][0]<=end){ //Overlap
             end=Math.max(end,intervals[i][1]);
-        }       
+        }
         else {
             list.add(new int[]{start,end});
-            start =intervals[i][0];
+            start=intervals[i][0];
             end=intervals[i][1];
         }
-        }
-        list.add(new int[]{start,end});
-        return list.toArray(new int[list.size()][]);
+     }
+    list.add(new int[]{start,end});
+    return list.toArray(new int[list.size()][]);
+
     }
 }
