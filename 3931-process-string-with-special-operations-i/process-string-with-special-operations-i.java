54219f3fd0b1dd206e968e1b3f0f190429489129
Vikash Kumar <138430675+vikashkumar016@users.noreply.github.com>
2026-06-18 00:51:22 +0530

    Time: 4 ms (76.30%) | Memory: 55.4 MB (48.89%) - LeetSync

---
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String processStr(String s) {
+        StringBuilder result = new StringBuilder();
+
+        for (char ch : s.toCharArray()) {
+
+            if (Character.isLowerCase(ch)) {
+                result.append(ch);
+            }
+            else if (ch == '*') {
+                if (result.length() > 0) {
+                    result.deleteCharAt(result.length() - 1);
+                }
+            }
+            else if (ch == '#') {
+                result.append(result.toString());
+            }
+            else if (ch == '%') {
+                result.reverse();
+            }
+        }
+
+        return result.toString();
+    }
+}
