f26cefc5358523a1a1b498aaae727bcf18a24827
Vikash Kumar <138430675+vikashkumar016@users.noreply.github.com>
2026-09-14 11:05:31 +0530

    Time: 0 ms (100.00%) | Memory: 45.4 MB (89.12%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.java
+++ 110-balanced-binary-tree/balanced-binary-tree.java
@@ -18,24 +18,20 @@ class Solution {
          return checkHeight(root) != -1;
     }
     private int checkHeight(TreeNode node){
-        if(node==null){
-            return 0;
-        }
-        int left=checkHeight(node.left); //left height
-        if(left==-1){
-            return -1;
-        }
-        int right =checkHeight(node.right);//right height
-        if(right==-1){
-            return -1;
-        }
-         if (Math.abs(left - right) > 1) {
-            return -1;
-        }
-         if(Math.abs(left-right)>1){
-            return -1;
-         }
-      return 1 + Math.max(left, right);
-
+       if(node==null){
+        return 0;
+       }
+       int leftHeight=checkHeight(node.left);
+       if(leftHeight==-1){
+        return -1;
+       }
+       int rightHeight=checkHeight(node.right);
+       if(rightHeight==-1){
+        return -1;
+       }
+       if(Math.abs(leftHeight-rightHeight)>1){
+        return -1;
+       }
+       return 1+Math.max(leftHeight,rightHeight);
     }
 }
